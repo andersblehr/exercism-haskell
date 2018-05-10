42c98d8716014510e489243da12f53a795b8ee61
Anders Blehr <anders@andersblehr.co>
2018-05-10 21:36:48 +0200

    Tighten solution for exercise 6: Collatz Conjecture

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,12 +1,8 @@
 module CollatzConjecture (collatz) where
 
-collatzSeries :: Integer -> [Integer]
-collatzSeries 1 = [1]
-collatzSeries n
-    | even n = n : collatzSeries (n `div` 2)
-    | odd n  = n : collatzSeries (3 * n + 1)
-
 collatz :: Integer -> Maybe Integer
+collatz 1 = Just 0
 collatz n
-    | n <= 0    = Nothing
-    | otherwise = Just $ subtract 1 $ toInteger . length $ collatzSeries n
+    | n < 1     = Nothing
+    | even n    = fmap (+ 1) $ collatz (div n 2)
+    | otherwise = fmap (+ 1) $ collatz (3 * n + 1)
