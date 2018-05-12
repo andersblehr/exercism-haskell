07c863c19e53648ce5b17ee4caa7d6d135d0a00a
Anders Blehr <anders@andersblehr.co>
2018-05-12 20:47:40 +0200

    Add exercise 11: Acronym

---
+++ acronym/src/Acronym.hs
@@ -0,0 +1,19 @@
+module Acronym (abbreviate) where
+
+import Data.Char (isLetter, isUpper, isLower, isPunctuation, isSpace, toUpper)
+
+isAcronymBoundary :: Char -> Char -> Bool
+isAcronymBoundary x y
+    | (isSpace x || isPunctuation x) && isLetter y = True
+    | isLower x && isUpper y                       = True
+    | otherwise                                    = False
+
+acronym :: String -> String -> String
+acronym acc (x:[])          = acc
+acronym acc (x:y:xys)
+    | acc == []             = acronym [toUpper x] ([y] ++ xys)
+    | isAcronymBoundary x y = acronym (acc ++ [toUpper y]) xys
+    | otherwise             = acronym acc ([y] ++ xys)
+
+abbreviate :: String -> String
+abbreviate = acronym []
