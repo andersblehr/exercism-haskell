bb331050f38a884272be31e1890d63752e3c7709
Anders Blehr <anders@andersblehr.co>
2018-05-17 13:13:38 +0200

    Add exercise 15: Grade School

---
+++ grade-school/src/School.hs
@@ -0,0 +1,26 @@
+module School (School, add, empty, grade, sorted) where
+
+import Data.List (insert)
+import qualified Data.Map as Map (fromList, lookup)
+
+type School = [(Int, [String])]
+
+add :: Int -> String -> School -> School
+add studentGrade student [] = [(studentGrade, [student])]
+add studentGrade student (record:records)
+    | studentGrade == grade = [(grade, insert student students)] ++ records
+    | studentGrade  < grade = [(studentGrade, [student])] ++ (record:records)
+    | otherwise             = [record] ++ add studentGrade student records
+    where grade    = fst record
+          students = snd record
+
+empty :: School
+empty = []
+
+grade :: Int -> School -> [String]
+grade gradeNum school = case Map.lookup gradeNum $ Map.fromList school of
+    Just students -> students
+    Nothing       -> []
+
+sorted :: School -> [(Int, [String])]
+sorted school = school
