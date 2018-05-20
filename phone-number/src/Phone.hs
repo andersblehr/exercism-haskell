9ed953d53f973194a19ab6cb88f931adfe736896
Anders Blehr <anders@andersblehr.co>
2018-05-20 08:43:13 +0200

    Add exercise 22: Phone Number

---
+++ phone-number/src/Phone.hs
@@ -0,0 +1,15 @@
+module Phone (number) where
+
+import Data.Char (isDigit, isLetter)
+
+number :: String -> Maybe String
+number cs
+    | containsLetters                           = Nothing
+    | length digits == 11 && head digits == '1' = number (tail digits)
+    | length digits == 10 && validDigits        = Just digits
+    | otherwise                                 = Nothing
+    where
+        containsLetters = foldl (\acc c -> acc || isLetter c) False cs
+        digits          = [c | c <- cs, isDigit c]
+        validDigits     = (not $ elem (head digits) ['0', '1']) &&
+                          (not $ elem (digits !! 3) ['0', '1'])
