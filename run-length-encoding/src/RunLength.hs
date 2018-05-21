898f5196ddbd6df19abf3ca0d2bbec3f7f17a91e
Anders Blehr <anders@andersblehr.co>
2018-05-21 09:20:35 +0200

    Iterate exercise 25

--- run-length-encoding/src/RunLength.hs
+++ run-length-encoding/src/RunLength.hs
@@ -3,26 +3,17 @@ module RunLength (decode, encode) where
 import Data.Char (isDigit)
 
 decode :: String -> String
-decode = snd . foldl (\acc c -> if isDigit c
-    then (fst acc ++ [c], snd acc)
-    else ([], snd acc ++ if length (fst acc) > 0
-        then (take (read (fst acc) :: Int) $ repeat c)
-        else [c])) ([], [])
-    
+decode [] = []
+decode encoded = replicate count char ++ decode (drop skip encoded)
+    where count   = if (not . null) digits then read digits else 1
+          char    = encoded !! (skip - 1)
+          skip    = length digits + 1
+          digits  = takeWhile isDigit encoded
+
 encode :: String -> String
 encode [] = []
-encode text = serialize $ reverse $ doEncode [(head text, 0)] text
-    where serialize crecs = concat $ map (\(c, count) -> if count == 1
-            then [c]
-            else show count ++ [c]) crecs
-
-doEncode :: [(Char, Int)] -> String -> [(Char, Int)]
-doEncode crecs (c : []) = encodeChar crecs c
-doEncode crecs (c : cs) = doEncode (encodeChar crecs c) cs
-
-encodeChar :: [(Char, Int)] -> Char -> [(Char, Int)]
-encodeChar all@(crec : crecs) c = if c == current
-    then ((c, succ count) : crecs)
-    else ((c, 1) : all)
-    where current = fst crec
-          count = snd crec
+encode text = count ++ [char] ++ encode (drop skip text)
+    where count = if skip > 1 then show skip else []
+          skip  = length chars
+          char  = head text
+          chars = takeWhile (== char) text
