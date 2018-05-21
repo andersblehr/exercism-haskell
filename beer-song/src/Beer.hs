4666a7f7d27393a4639e9511cc352e5796f677ef
Anders Blehr <anders@andersblehr.co>
2018-05-21 10:35:17 +0200

    Iterate exercise 26

--- beer-song/src/Beer.hs
+++ beer-song/src/Beer.hs
@@ -1,13 +1,14 @@
 module Beer (song) where
 
+import Data.List (intercalate)
+
 song :: String
-song = foldl (\acc n -> acc ++ line n) [] [99, 98..0]
+song = intercalate "\n" $ map verse [99, 98..0]
     
-line :: Int -> String
-line n
+verse :: Int -> String
+verse n
     | n > 0     = bottles n ++ " of beer on the wall, " ++ bottles n ++ " of beer.\n\
-                  \Take " ++ one n ++ " down and pass it around, " ++ bottles (n - 1) ++ " of beer on the wall.\n\
-                  \\n"
+                  \Take " ++ one n ++ " down and pass it around, " ++ bottles (n - 1) ++ " of beer on the wall.\n"
     | otherwise = "No more bottles of beer on the wall, no more bottles of beer.\n\
                   \Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     where bottles n
