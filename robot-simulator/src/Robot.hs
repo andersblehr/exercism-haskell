d2f1ca8eec9e86605d7350e8aa58d4eaeb549e3a
Anders Blehr <anders@andersblehr.co>
2018-05-22 20:43:52 +0200

    Iterate exercise 31

--- robot-simulator/src/Robot.hs
+++ robot-simulator/src/Robot.hs
@@ -28,19 +28,17 @@ mkRobot direction coordinates = (coordinates, direction)
 simulate :: Robot -> String -> Robot
 simulate = foldl step
     where step robot instruction
-            | instruction == 'R' = mkRobot (turnRight $ bearing robot) (coordinates robot)
-            | instruction == 'L' = mkRobot (turnLeft $ bearing robot) (coordinates robot)
-            | instruction == 'A' = advance robot
-          advance robot = case direction of
-            North -> mkRobot direction (x, y + 1)
-            East  -> mkRobot direction (x + 1, y)
-            South -> mkRobot direction (x, y - 1)
-            West  -> mkRobot direction (x - 1, y)
+            | instruction == 'R' = mkRobot (turnRight direction) position
+            | instruction == 'L' = mkRobot (turnLeft direction) position
+            | instruction == 'A' = advance position
             where direction = bearing robot
-                  x         = fst position
-                  y         = snd position
-                  position  = coordinates robot
-
+                  position = coordinates robot
+                  advance (x, y) = case direction of
+                    North -> mkRobot direction (x, y + 1)
+                    East  -> mkRobot direction (x + 1, y)
+                    South -> mkRobot direction (x, y - 1)
+                    West  -> mkRobot direction (x - 1, y)
+                  
 turnLeft :: Bearing -> Bearing
 turnLeft North = West
 turnLeft East  = North
