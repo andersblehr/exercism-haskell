90cabc5b74effb3bf3c363f180f5be0f9f605d3c
Anders Blehr <anders@andersblehr.co>
2018-05-23 20:49:36 +0200

    Iterate exercise 32

--- secret-handshake/src/SecretHandshake.hs
+++ secret-handshake/src/SecretHandshake.hs
@@ -1,24 +1,15 @@
 module SecretHandshake (handshake) where
 
-import qualified Data.Map as Map (fromList, lookup)
-import Data.Maybe (fromMaybe)
-
-reportoire = Map.fromList [ (    1, "wink"           )
-                          , (   10, "double blink"   )
-                          , (  100, "close your eyes")
-                          , ( 1000, "jump"           )
-                          , (10000, "reverse"        )
-                          ]
+reportoire = ["wink", "double blink", "close your eyes", "jump", "reverse"]
 
 handshake :: Int -> [String]
-handshake n = foldl addMove [] (bits n 1)
-    where addMove moves bit
-            | null nextMove         = moves
-            | nextMove == "reverse" = reverse moves
-            | otherwise             = moves ++ [nextMove]
-            where nextMove = fromMaybe [] $ Map.lookup bit reportoire
+handshake n = foldl addMove [] $ zip (bits n) reportoire
+    where addMove moves (bit, move)
+            | bit == 0          = moves
+            | move == "reverse" = reverse moves
+            | otherwise         = moves ++ [move]
 
-bits :: Int -> Int -> [Int]
-bits 0 _ = []
-bits n factor = factor * rem n 2 : bits (quot n 2) (factor * 10)
+bits :: Int -> [Int]
+bits 0 = []
+bits n = rem n 2 : bits (quot n 2)
           
