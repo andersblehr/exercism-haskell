1efa4d9751301d00d7e706583409d823964efa7b
Anders Blehr <anders@andersblehr.co>
2018-05-27 15:46:08 +0200

    Iterate exercise 24

--- isbn-verifier/src/IsbnVerifier.hs
+++ isbn-verifier/src/IsbnVerifier.hs
@@ -7,7 +7,7 @@ isbn = checkChecksum . hasDigits . hasChecksum . hasLength . stripDashes
     where stripDashes      = filter (/= '-')
           hasLength s      = if length s == 10 then s else "-"
           hasChecksum s    = if isChecksum . head $ reverse s then s else "-"
-          isChecksum d     = if isDigit d || d == 'X' then True else False
+          isChecksum d     = isDigit d || d == 'X'
           hasDigits s      = if isAllDigits . tail $ reverse s then s else "-"
           isAllDigits      = foldl (\acc d -> acc && isDigit d) True
           checkChecksum    = (== 0) . flip mod 11 . foldl partSum 0 . zip [1..]
