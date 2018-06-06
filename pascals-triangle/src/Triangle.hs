04216f63d28cbe9e9f2a5db52b48d6b26ad7957c
Anders Blehr <anders@andersblehr.co>
2018-06-06 21:15:14 +0200

    Iterate exercise 46

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -1,17 +1,6 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows 0 = []
-rows x = foldl (\rows' _ -> rows' ++ [next (if null rows'
-                                            then []
-                                            else last rows')]) [] [1..x]
-
-next :: [Integer] -> [Integer]
-next []  = [1]
-next row = map (aggregate row) [0..length row]
-
-aggregate :: [Integer] -> Int -> Integer
-aggregate _ 0     = 1
-aggregate row pos = if pos < length row
-                    then row !! (pos - 1) + row !! pos
-                    else 1
+rows n = take n pascal where
+    pascal = [1] : map next pascal
+    next row = zipWith (+) (0 : row) (row ++ [0])
