f697d2f486e384ea11c6c7e6c6788af84ccfe5c1
Anders Blehr <anders@andersblehr.co>
2018-06-07 19:00:42 +0200

    Add exercise 47: Pythagorean Triplets

---
+++ pythagorean-triplet/src/Triplet.hs
@@ -0,0 +1,18 @@
+module Triplet (isPythagorean, mkTriplet, pythagoreanTriplets) where
+
+isPythagorean :: (Int, Int, Int) -> Bool
+isPythagorean (a, b, c) = a^2 + b^2 == c^2
+
+mkTriplet :: Int -> Int -> Int -> (Int, Int, Int)
+mkTriplet a b c
+    | isPythagorean (a, b, c) = (a, b, c)
+    | isPythagorean (b, c, a) = (b, c, a)
+    | otherwise               = (c, a, b)
+
+pythagoreanTriplets :: Int -> Int -> [(Int, Int, Int)]
+pythagoreanTriplets min max = [(a, b, c) | a <- [min..max]
+                                         , b <- [min..max]
+                                         , c <- [min..max]
+                                         , b >= a
+                                         , c >= c
+                                         , a^2 + b^2 == c^2]
