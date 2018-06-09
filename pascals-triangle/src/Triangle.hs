c331bbad3e98bf64d670935adf031a8d6cb736da
Anders Blehr <anders@andersblehr.co>
2018-06-09 14:20:17 +0200

    Iterate exercise 46

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -1,6 +1,4 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows n = take n pascal where
-    pascal = [1] : map next pascal
-    next row = zipWith (+) (0 : row) (row ++ [0])
+rows n = take n $ iterate (\row -> zipWith (+) (0 : row) (row ++ [0])) [1]
